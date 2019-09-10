d16a37927fa56dc632327bc606b76738ba9cf419
Mateusz Choma <chomamateusz@gmail.com>
2019-09-10 18:29:09 +0200

    monorepo structure

---
+++ app2/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 9000
+
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>APP 2</h1>
+    <h2>Hello World from Docker and Node.js! 0.0.14!</h2>
+  `)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
