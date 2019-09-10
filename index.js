4005f33ffc3022ae2c125424bc942d25f4cff8bc
Mateusz Choma <chomamateusz@gmail.com>
2019-09-10 13:38:28 +0200

    0.0.5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 9000;
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World from Docker and Node.js! 0.0.1!</h1>');
+    res.send('<h1>Hello World from Docker and Node.js! 0.0.5!</h1>');
 });
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
